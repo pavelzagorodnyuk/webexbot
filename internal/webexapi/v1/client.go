728da2f53f872d3ab917ebedb506b17806f7ac7d
Pavel Zagorodnyuk <pavel.zagorodnyuk@gmail.com>
2024-02-12 20:18:46 +0300

    feat: add Webex client definition

---
+++ internal/webexapi/v1/client.go
@@ -0,0 +1,52 @@
+package webexapi
+
+import "net/http"
+
+// Client provides methods to interact with the Webex API
+type Client interface{}
+
+// client is an implementation of the Client interface
+type client struct {
+	webexURL   string
+	authToken  string
+	httpClient http.Client
+}
+
+// DefaultWebexURL is used as the default URL to the Webex API
+const DefaultWebexURL = "https://webexapis.com/v1"
+
+// DefaultHTTPClient is used as the default HTTP client for making requests to the Webex API
+var DefaultHTTPClient = http.Client{}
+
+// NewClient creates a new instance of the Client with the specified authentication token. It uses the DefaultWebexURL
+// as a URL to the Webex API and the DefaultHTTPClient as an HTTP client for making requests to the API. If it is
+// needed, these parameters could be redefined by options.
+func NewClient(token string, options ...Option) Client {
+	client := &client{
+		webexURL:   DefaultWebexURL,
+		authToken:  token,
+		httpClient: DefaultHTTPClient,
+	}
+
+	for _, option := range options {
+		option(client)
+	}
+	return client
+}
+
+// Option configures the Client behavior
+type Option func(*client)
+
+// WithHTTPClient creates an option which defines an HTTP client used by the Client
+func WithHTTPClient(httpClient http.Client) Option {
+	return func(c *client) {
+		c.httpClient = httpClient
+	}
+}
+
+// WithWebexURL creates an option which defines a Webex API URL used by the Client
+func WithWebexURL(webexURL string) Option {
+	return func(c *client) {
+		c.webexURL = webexURL
+	}
+}
