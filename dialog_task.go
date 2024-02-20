54d68aa72185f68dabd240d04e4fa18fa32296a2
Pavel Zagorodnyuk <pavel.zagorodnyuk@gmail.com>
2024-02-20 20:20:48 +0300

    feat: implement some dialog task providers

--- dialog_task.go
+++ dialog_task.go
@@ -11,3 +11,34 @@ type DialogTaskProvider interface {
 	// dialog task
 	ProvideFor(Event) DialogTask
 }
+
+// DialogTaskProviderFunc allows to use ordinary functions which implements DialogTaskProvider.ProvideFor method as
+// dialog task providers
+type DialogTaskProviderFunc func(Event) DialogTask
+
+func (p DialogTaskProviderFunc) ProvideFor(event Event) DialogTask {
+	return p(event)
+}
+
+type combinedDialogTaskProvider struct {
+	subproviders []DialogTaskProvider
+}
+
+// NewCombinedDialogTaskProvider creates a new dialog task provider which combines the specified dialog task providers.
+// It calls each subprovider and returns the first received dialog task. If all subproviders return nil, it returns nil
+// too.
+func NewCombinedDialogTaskProvider(subproviders ...DialogTaskProvider) DialogTaskProvider {
+	return &combinedDialogTaskProvider{
+		subproviders: subproviders,
+	}
+}
+
+func (p combinedDialogTaskProvider) ProvideFor(event Event) DialogTask {
+	for _, subprovider := range p.subproviders {
+		triggeredTask := subprovider.ProvideFor(event)
+		if triggeredTask != nil {
+			return triggeredTask
+		}
+	}
+	return nil
+}
